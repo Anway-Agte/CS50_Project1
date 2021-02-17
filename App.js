a51dd13c03b9bb8271e37ec624762a88918dc9bf
SteveMaverick <anway.agte@gmail.com>
2021-02-17 15:42:10 +0530

    final commit

--- App.js
+++ App.js
@@ -1,144 +1,120 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View , Button, TextPropTypes, VirtualizedList } from 'react-native';
+import { StyleSheet, Text, View , Button, TextPropTypes, VirtualizedList ,Vibration} from 'react-native';
+
+
+class Counter extends React.Component{
+  shouldComponentUpdate(nextProps){
+    return (nextProps.data.workTime >= 0)
+  }
+  render(){
+    const time = this.props.data.workModeActive ? this.props.data.workTime : this.props.data.restTime 
+    return(
+      <Text style = {styles.counter}>{Math.floor(time / 60)} m : {time % 60} s</Text>
+    )
+  }
+}
 
 class Timer extends React.Component{
-  shouldComponentUpdate(nextProps){
-    
-    return (nextProps.workTimeInSeconds > 0)
-  }
-  render(){
-    return(
-      <Text style = {this.props.style}>{Math.floor(this.props.time / 60)}:{this.props.time% 60 }</Text>
-    )
-  }
-}
- 
-class Counter extends React.Component{
-  
-  constructor(props){
-    super(props) ; 
+  constructor(){
+    super()
     this.state = {
-      workTimeInSeconds : 10 , 
-      restTimeInSeconds : 60 ,
+      workTime : 300, 
+      restTime : 60 , 
+      mode : "Work Mode",
+      workModeActive : true , 
+      paused : false , 
     }
-    
   }
-
-  initiateTimer(){
-    this.timer = setInterval(()=>{this.setState(prevState => ({workTimeInSeconds : prevState.workTimeInSeconds - 1})) ;} , 1000)
-  }
-
   componentDidMount(){
-   this.timer = setInterval(this.startWorkTimer , 1000)
+    this.timer = setInterval(this.startTimer , 1000)
+  }
+  startTimer = () =>{
+    if(this.state.workModeActive){
+    this.setState(prevState => ({workTime : prevState.workTime - 1}))
+    if(this.state.workTime === 0 ){
+      Vibration.vibrate([500, 500, 500])
+    }
+    if(this.state.workTime === -1){
+      Vibration.vibrate([500, 500, 500])
+      this.setState({workModeActive : false}) 
+      this.setState({ workTime : 300})
+      
+    }
+    }
+    else{
+      this.setState(prevState => ({ restTime : prevState.restTime - 1}))
+      if(this.state.restTime === 0){
+        Vibration.vibrate([500, 500, 500])
+      }
+      if(this.state.restTime === -1){
+        this.setState({workModeActive : true}) 
+        this.setState({ restTime : 60})
+      }
+    }
+  }
+  reset = ()=> {
+    this.setState({workTime : 300, 
+      restTime : 60 , 
+      mode : "Work Mode",
+      workModeActive : true , 
+      paused : true , })
+      clearInterval(this.timer)
   }
 
-  shouldComponentUpdate(nextProps){
-    return !(nextProps.workTimeInSeconds % 2)
-  }
-
-  startWorkTimer = ()=> {
-    this.setState(prevState => ({workTimeInSeconds : prevState.workTimeInSeconds - 1})) ; 
-  }
-  
-  render(){
-      return( 
-       
-        <Timer style = {styles.timer} time = {this.state.workTimeInSeconds}></Timer> )
-  }
-}
-
-class Mode extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      mode : "Work Mode"
+  startPause = () =>{
+    if(!this.state.paused){
+      clearInterval(this.timer) 
+      this.setState({paused : true})
+    }
+    else{
+      this.timer = setInterval(this.startTimer , 1000)
+      this.setState({paused : false})
     }
   }
   render(){
     return(
-      <Text style = {{fontSize:35 , }}>{this.state.mode}</Text>
-    )
-  }
-}
-
-export default class App extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      counterActive : false 
-    }
-  }
-
-  startTimer(){
-    this.setState({
-      counterActive:true 
-    }) 
-  }
-  render(){ 
-    if(!this.state.counterActive){
-      return(
-        <View style={styles.container}>
-        <StatusBar style="auto" />
-        <View style = {styles.buttondiv}>
+    <React.Fragment>
+      <View style = {styles.container}>
+      <Text style = {styles.mode}>{this.state.workModeActive ? "Work timer" : "Break Timer"}</Text>
+      <Counter data = {this.state}></Counter>
+      <View style = {styles.buttondiv}>
           <View style = {styles.button}>
-            <Button onPress  = {this.startTimer.bind(this)} title = "Start"></Button>
+            <Button onPress = {this.startPause} title = {this.state.paused ? "Start" : "Pause"}></Button>
           </View>
           <View style = {styles.button}>
-            <Button color = "#FF0000"  title = "Stop"></Button>
-          </View>
-          <View style = {styles.button}>
-            <Button title = "Reset"></Button>
+            <Button onPress = {this.reset} title = "Reset"></Button>
           </View>
         
         </View>
       </View>
-
-      )
-    }
-    else{
-      return (
-    <View style={styles.container}>
-      <Mode></Mode>
-      <Counter></Counter>
-      <StatusBar style="auto" />
-      <View style = {styles.buttondiv}>
-        <View style = {styles.button}>
-          <Button onPress  = {this.startTimer.bind(this)} title = "Start"></Button>
-        </View>
-        <View style = {styles.button}>
-          <Button color = "#FF0000"  title = "Stop"></Button>
-        </View>
-        <View style = {styles.button}>
-          <Button title = "Reset"></Button>
-        </View>
-      
-      </View>
-    </View>
-  );
-    }
-} 
+    </React.Fragment>
+    )
+  }
 }
 
+export default  class App extends React.Component{
+render( ){
+  return (
+    <Timer></Timer>
+  )
+}
 
+} 
 
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-   
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  timer : {
-   marginVertical:20 ,
-   color: "#841584" , 
-   fontSize : 25 , 
-  } , 
+  container :{
+    flex :1 ,
+    alignItems : 'center' , 
+    justifyContent : 'center' , 
+  } ,
+  mode:{
+    fontSize : 35 
+
+  } ,
   buttondiv:{
     flexDirection:'row',
     justifyContent:'space-between' , 
@@ -147,5 +123,10 @@ const styles = StyleSheet.create({
     marginHorizontal : 5 ,
     height : 100 , 
     width : 100 ,
+  } , 
+  counter : {
+    fontSize : 45 , 
+    color: "#841584" , 
+    marginVertical :10 ,
   }
-});
+})
